b70eba75f628e0da46e823e46f49f287ec9c778a
denys <1995gdv@gmail.com>
2018-03-24 18:41:23 +0200

    Install ng-semantic. Add tabs to lessons

--- src/app/modules/lessons/lessons.module.ts
+++ src/app/modules/lessons/lessons.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LessonsComponent} from './components/lessons/lessons.component';
+import {SuiModule} from 'ng2-semantic-ui';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SuiModule
   ],
   declarations: [LessonsComponent],
   exports: [LessonsComponent]
