35834ee052bd52aa60bfcf1745d493ec411320bf
denys <1995gdv@gmail.com>
2018-09-18 14:51:02 +0300

    Add search field to chat

--- src/app/modules/auth/components/signup/signup.component.ts
+++ src/app/modules/auth/components/signup/signup.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {environment} from '../../../../../environments/environment';
 import {AuthService} from '../../auth.service';
 
 @Component({
